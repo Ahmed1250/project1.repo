ce047c3fae96a5ba068a2a438e3ef57f472b440c
shohel ahmed <shohelahmed893@gmail.com>
2022-01-16 16:10:27 +0600

    added all

---
+++ project/index.js
@@ -0,0 +1,13 @@
+var menuBtn = document.getElementById("menuBtn");
+var sideMenu = document.getElementById("sideMenu");
+
+
+sideMenu.style.right = "-250px";
+menuBtn.onclick = function(){
+    if(sideMenu.style.right == "-250px"){
+        sideMenu.style.right = "0";
+    }
+    else{
+        sideMenu.style.right = "-250px";
+    }
+}
